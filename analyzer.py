86ada4c213f5c5d58a235283440d98ab3a488699
shhj1998 <shhj1998@postech.ac.kr>
2020-05-21 12:54:11 +0900

    Add correlation matrix plot

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,11 @@ import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import torch
+from sklearn.preprocessing import PolynomialFeatures
 
-from dataset import load_dataset
+from dataset import CatheterDataset
 
 
 def histogram_plot(data, idx):
@@ -28,23 +30,31 @@ args = parser.parse_args()
 
 torch.manual_seed(args.seed)
 
-datasets = load_dataset()
-titles = ['Train', 'Validation', 'Test']
+dataset = CatheterDataset()
+transformer = PolynomialFeatures(3)
 
-for idx, dataset in enumerate(datasets):
-    print(f'# {titles[idx]} set')
+for i in range(3):
+    print(f'## Sensor {i}')
+    print(f'### Mean: {np.mean(dataset[:][0][:, i])}')
+    print(f'### Std: {np.std(dataset[:][0][:, i])}\n')
 
-    for i in range(3):
-        print(f'## Sensor {i}')
-        print(f'### Mean: {np.mean(dataset[:][0][:, i])}')
-        print(f'### Std: {np.std(dataset[:][0][:, i])}\n')
+plt.figure(1)
+for i in range(3):
+    histogram_plot(dataset, i + 1)
 
-    plt.figure(2 * idx + 1)
-    for i in range(3):
-        histogram_plot(dataset, i + 1)
+plt.figure(2)
+for i in range(3):
+    box_plot(dataset, i + 1)
 
-    plt.figure(2 * idx + 2)
-    for i in range(3):
-        box_plot(dataset, i + 1)
+plt.figure(3)
+x, y = dataset[:][0], dataset[:][1]
+x = transformer.fit_transform(x)
+data = np.hstack((x, np.atleast_2d(y).T))
+df = pd.DataFrame(data)
+corr = df.corr()
+plt.matshow(df.corr())
+plt.xticks(range(len(df.columns)), df.columns)
+plt.yticks(range(len(df.columns)), df.columns)
+plt.colorbar()
 
 plt.show()
