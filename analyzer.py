e491817a5cadad6691e815975600dc371e4a0a09
shhj1998 <shhj1998@postech.ac.kr>
2020-05-29 23:03:59 +0900

    Fixed analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,62 +1,45 @@
-import argparse
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import torch
-from sklearn.preprocessing import PolynomialFeatures
 
 from dataset import CatheterDataset
 
 
-def histogram_plot(data, idx):
-    plt.subplot(3, 1, idx)
-    plt.hist(data[:][0][:, idx - 1], bins=1000)
-    plt.title(f'Sensor{idx}')
-    plt.xlabel('\u0394\u03BB')
-    plt.ylabel('frequency')
+def histogram_plot(data, title, label):
+    plt.title(title)
+    plt.hist(data, bins=1000)
+    plt.xlabel(label)
 
 
-def box_plot(data, idx):
-    plt.subplot(1, 3, idx)
-    plt.boxplot(data[:][0][:, idx - 1])
-    plt.title(f'Sensor{idx + 1}')
+def box_plot(data, title):
+    plt.boxplot(data, vert=False)
+    plt.title(title)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Smart Catheter Trainer')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    args = parser.parse_args()
-
-    torch.manual_seed(args.seed)
-
+    torch.manual_seed(1)
     dataset = CatheterDataset()
-    transformer = PolynomialFeatures(3)
 
     for i in range(3):
-        print(f'## Sensor {i}')
-        print(f'### Mean: {np.mean(dataset[:][0][:, i])}')
-        print(f'### Std: {np.std(dataset[:][0][:, i])}\n')
+        print(f'<Sensor {i}>')
+        print(f'- Mean: {np.mean(dataset[:][0][:, i])}')
+        print(f'- Std: {np.std(dataset[:][0][:, i])}\n')
 
     plt.figure(1)
     for i in range(3):
-        histogram_plot(dataset, i + 1)
+        plt.subplot(3, 1, i + 1)
+        histogram_plot(dataset[:][0][:, i], f'Sensor{i + 1}', '\u0394\u03BB')
 
     plt.figure(2)
     for i in range(3):
-        box_plot(dataset, i + 1)
+        plt.subplot(3, 1, i + 1)
+        box_plot(dataset[:][0][:, i], f'Sensor{i + 1}')
 
     plt.figure(3)
-    x, y = dataset[:][0], dataset[:][1].reshape(-1)
-    x = transformer.fit_transform(x)
-    data = np.hstack((x, np.atleast_2d(y).T))
-    df = pd.DataFrame(data)
-    corr = df.corr()
-    plt.matshow(df.corr())
-    plt.xticks(range(len(df.columns)), df.columns)
-    plt.yticks(range(len(df.columns)), df.columns)
-    plt.colorbar()
+    plt.subplot(2, 1, 1)
+    histogram_plot(dataset[:][1], 'Force', 'N')
+    plt.subplot(2, 1, 2)
+    box_plot(dataset[:][1], 'Force')
 
     plt.show()
 
