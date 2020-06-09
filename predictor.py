636d334a3ee0c717c0ee8649b8bd195bae8439cd
shhj1998 <shhj1998@postech.ac.kr>
2020-06-09 19:47:08 +0900

    Add predictor and fixed testing for rnn

--- predictor.py
+++ predictor.py
@@ -1,78 +1,34 @@
-import argparse
-from pathlib import Path
-
-import matplotlib.pyplot as plt
+import numpy as np
+import pywt
 import torch
-from torch.nn.functional import l1_loss
-
-from models import DNN, RNN, CNN
-from data import import_data
-from preprocess import ndata, frequency
+from scipy.interpolate import interp1d
 
 
-def repackage_hidden(h):
-    if h is None:
-        return h
-    if isinstance(h, torch.Tensor):
-        return h.detach()
-    else:
-        return tuple(repackage_hidden(v) for v in h)
+class Predictor:
+    """
+    model : model file for prediction. (e.g. checkpoints/test/model.pth)
+    device : if you want to use gpu, set to 'gpu'
+    """
 
+    def __init__(self, model, device='cpu'):
+        self.device = device
+        self.model = torch.load(model, map_location='cpu').to(device)
 
-def main():
-    parser = argparse.ArgumentParser(description='Smart Catheter Predictor')
-    parser.add_argument('--file-name', type=str, default='checkpoints/test/checkpoint_final.pth')
-    parser.add_argument('--result-dir', type=str, default='results/test')
-    parser.add_argument('--no-cuda', action='store_true', default=False)
-    parser.add_argument('--batch-size', type=int, default=256)
-    args = parser.parse_args()
+    """
+    x : numpy array shape of (N, 3).
+    t : time array shape of (N). Must be strictly ascending order
+    """
 
-    torch.manual_seed(1)
-    Path(args.result_dir).mkdir(parents=True, exist_ok=True)
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device('cuda' if use_cuda else 'cpu')
-    print(f'Device selected: {device}\n')
+    def predict(self, x, t):
+        t -= t[0]
+        interpolator = interp1d(t, x, axis=0)
+        x = interpolator(np.arange(0, 0.1, 0.001))
+        input = np.empty((100, 100, 3))
 
-    model = torch.load(args.file_name, map_location='cpu').to(device)
-    model.eval()
-    total_loss = 0.0
+        for channel in range(3):
+            result, _ = pywt.cwt(x[:, channel], range(1, 101), 'morl', 0.001)
+            input[:, :, channel] = result
 
-    with torch.no_grad():
-        root_dir = 'data/preprocess/' + ('spectrogram' if model.type == 'CNN' else 'series')
-        x, y = import_data(root_dir, 'test', model.type)
-        if model.type == 'RNN':
-            x, y = x.transpose((1, 0, 2)), y.transpose((1, 0, 2))
-        sz = int(y.shape[0] / ndata['test'])
-
-        for idx in range(ndata['test']):
-            if model.type == 'RNN':
-                x_data, y_real = x[idx], y[idx]
-            else:
-                x_data, y_real = x[idx*sz:(idx + 1)*sz], y[idx*sz:(idx + 1)*sz]
-            x_data, y_real = torch.from_numpy(x_data).to(device), torch.from_numpy(y_real).to(device)
-
-            if model.type == 'RNN':
-                h = model.init_hidden(ndata['test']).to(device)
-                h = repackage_hidden(h)
-                y_pred, _ = model(x_data, h)
-            else:
-                y_pred = model(x_data)
-
-            y_real, y_pred = y_real.cpu(), y_pred.cpu()
-            xrange = range(0, y_real.shape[0])
-
-            loss = l1_loss(y_real, y_pred)
-            total_loss += loss.item()
-            print(f'Record #{idx + 1} L1 Loss : {loss}')
-            
-            plt.plot(xrange, y_real.data, label='real value')
-            plt.plot(xrange, y_pred.data, label='prediction')
-            plt.ylabel('Weight [g]')
-            plt.legend(loc=2)
-            plt.savefig(f'{args.result_dir}/prediction-{idx + 1}.png', dpi=300)
-            plt.cla()
-        
-        print(f'Total Test L1 Loss : {total_loss / ndata["test"]}')
-
-
-main()
+        input = torch.from_numpy(input.transpose((2, 0, 1))).to(self.device)
+        print(input.shape)
+        return self.model(input).data
---
+++ test.py
@@ -0,0 +1,77 @@
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+from torch.nn.functional import l1_loss
+
+from data import import_data
+from preprocess import ndata
+
+
+def repackage_hidden(h):
+    if h is None:
+        return h
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Smart Catheter Predictor')
+    parser.add_argument('--file-name', type=str, default='checkpoints/test/checkpoint_final.pth')
+    parser.add_argument('--result-dir', type=str, default='results/test')
+    parser.add_argument('--no-cuda', action='store_true', default=False)
+    parser.add_argument('--batch-size', type=int, default=256)
+    args = parser.parse_args()
+
+    torch.manual_seed(1)
+    Path(args.result_dir).mkdir(parents=True, exist_ok=True)
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device('cuda' if use_cuda else 'cpu')
+    print(f'Device selected: {device}\n')
+
+    model = torch.load(args.file_name, map_location='cpu').to(device)
+    model.eval()
+    total_loss = 0.0
+
+    with torch.no_grad():
+        root_dir = 'data/preprocess/' + ('spectrogram' if model.type == 'CNN' else 'series')
+        x, y = import_data(root_dir, 'test', model.type)
+        if model.type == 'RNN':
+            x, y = x.transpose((1, 0, 2)), y.transpose((1, 0, 2))
+        sz = int(y.shape[0] / ndata['test'])
+
+        for idx in range(ndata['test']):
+            if model.type == 'RNN':
+                x_data, y_real = x[:, idx].reshape((1, -1, 3)), y[:, idx]
+            else:
+                x_data, y_real = x[idx * sz:(idx + 1) * sz], y[idx * sz:(idx + 1) * sz]
+            x_data, y_real = torch.from_numpy(x_data).to(device), torch.from_numpy(y_real).to(device)
+
+            if model.type == 'RNN':
+                h = model.init_hidden(1).to(device)
+                h = repackage_hidden(h)
+                y_pred, _ = model(x_data, h)
+            else:
+                y_pred = model(x_data)
+
+            y_real, y_pred = y_real.cpu().view(-1), y_pred.cpu().view(-1)
+            xrange = range(0, y_real.shape[0])
+
+            loss = l1_loss(y_real, y_pred)
+            total_loss += loss.item()
+            print(f'Record #{idx + 1} L1 Loss : {loss}')
+
+            plt.plot(xrange, y_real.data, label='real value')
+            plt.plot(xrange, y_pred.data, label='prediction')
+            plt.ylabel('Weight [g]')
+            plt.legend(loc=2)
+            plt.savefig(f'{args.result_dir}/prediction-{idx + 1}.png', dpi=300)
+            plt.cla()
+
+        print(f'Total Test L1 Loss : {total_loss / ndata["test"]}')
+
+
+main()
